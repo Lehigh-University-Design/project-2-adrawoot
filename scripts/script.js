93d2e89688a0e95818fa5d19371fd8648243e612
“adrawoot” <adrawoot@gmail.com>
2024-03-06 09:27:50 -0500

    Merge branch 'main' of https://github.com/Lehigh-University-Design/project-2-adrawoot

--- scripts/script.js
+++
@@ -1,9 +0,0 @@
-function nextPageFunction() {
-    document.getElementsByClassName("step").style.transform="translateX(100vw)";
-    document.getElementsByClassName("step").style.transform="all 2s";
-}
-
-function previousPageFunction() {
-    document.getElementsByClassName("step").style.transform="translateX(-100vw)";
-    document.getElementsByClassName("step").style.transform="all 2s";
-}
